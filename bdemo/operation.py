06b674d4881284ebfe791298aa2c3216f4010d27
Nathanaël Jourdane <njourdane@protonmail.com>
2025-01-09 15:16:41 +0100

    add operation unique identier

--- bdemo/operation.py
+++ bdemo/operation.py
@@ -2,11 +2,15 @@ from __future__ import annotations
 
 import build123d as _
 
+operations = {}
+
 
 class Operation:
     def __init__(self, obj: _.Part, name: str, last: Operation|None) -> None:
         self.name = name
         self.last = last
+        operations[name] = operations[name] + 1 if name in operations else 1
+        self.id = f"{ name }-{ operations[name] }"
 
         self.faces = {self.get_id(f): f for f in obj.faces()}
         self.edges = {self.get_id(e): e for e in obj.edges()}
@@ -14,7 +18,7 @@ class Operation:
         self.shapes = {**self.faces, **self.edges}
 
     def __repr__(self):
-        return self.name
+        return self.id
 
     def get_id(self, shape: _.Shape) -> str:
         return hex(hash(shape))[2:]
--- examples/demo.py
+++ examples/demo.py
@@ -1,11 +1,13 @@
 from bdemo.part_primitives import Box, Cylinder
 
 obj = Box(9, 9, 3)
-obj.add(Box(3, 3, 6))
-obj.sub(Cylinder(1, 6))
+obj.add(Box(6, 6, 6))
+obj.add(Box(3, 3, 9))
+obj.sub(Cylinder(1, 9))
 
 for operation in obj.operations:
-    print(operation, list(operation.faces.keys()))
+    # print(operation, list(operation.faces.keys()))
+    print(operation)
 
 if __name__ == "__main__":
     import ocp_vscode as ov
